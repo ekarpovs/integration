026e02b608f9b181469417fd428a0092ab000006
evgeny <ekarpovsky@gmail.com>
2025-02-12 17:28:59 +0000

    change disconnect flow

--- testing/tools/socketio/sio-client.py
+++ testing/tools/socketio/sio-client.py
@@ -11,8 +11,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-owner_type = os.getenv('OWNER_TYPE')
-owner = os.getenv('OWNER', '2974528d-155d-42ed-aa01-37767a1994f8')
+namespace = os.getenv('NAMESPACE')
+room = os.getenv('ROOM', '2974528d-155d-42ed-aa01-37767a1994f8')
 bs_url= os.getenv('BROADCAST_SERVICE_URL', 'ws://127.0.0.1:8020')
 sio_path = os.getenv('SIO_PATH', '/ws/socket.io')
 transports = os.getenv('TRANSPORTS', ['websocket', 'polling', 'webtransport']) 
@@ -24,7 +24,7 @@ sio = socketio.AsyncClient(
     reconnection_attempts=reconnection_attempts
 )
 
-namespace = f'/{owner_type}'
+namespace = f'/{namespace}'
 
 @sio.on("server-message", namespace=namespace)
 async def get_from_server(data):
@@ -32,16 +32,18 @@ async def get_from_server(data):
     Get `server-message` event from server and print the log.
     """
     print(f'The message `{data}` was received from the server.')
-    # if data == 'cancel':
-    #     print("stopp via message - `cancel`")
-    #     sys.exit()
+    if data == 'cancel':
+        # Initiate the disconnect event
+        await sio.emit('leave_room', namespace=namespace)
+        # await sio.disconnect()
+        # sys.exit()
 
 
 
 @sio.on('connect', namespace=namespace)
 async def handle_successful_connect():
     print(f"Connected:")
-    await sio.emit('join_room', owner, namespace=namespace)
+    await sio.emit('join_room', room, namespace=namespace)
 
 @sio.on("connect_error")
 async def handle_connect_error(data):
@@ -54,7 +56,7 @@ async def handle_connect_error(data):
 @sio.on('disconnect', namespace=namespace)
 async def handle_disconnect():
     print("Disconnected from server")
-    await sio.emit('leave_room', 'room1', namespace=namespace)
+    await sio.emit('leave_room', namespace=namespace)
                    
 
 @sio.on('room_joined', namespace=namespace)
@@ -64,6 +66,7 @@ async def room_joined(room):
 @sio.on('room_left', namespace=namespace)
 async def room_left(room):
     print(f"Left room: {room}")
+    await sio.disconnect()
 
 
 async def main():
@@ -72,19 +75,16 @@ async def main():
     """
     auth = {
         'owner-type': namespace,
-        'owner': owner,
+        'owner': room,
         'client': str(uuid.uuid1()),
     }
     try:
         await sio.connect(bs_url, socketio_path=sio_path, auth=auth, transports=transports, namespaces=[namespace])
     except exceptions.ConnectionError:
         print('Can`t connect to the server')
-        await sio.emit('leave_room', owner, namespace=namespace)
         exit()
 
     try:
-        # while True:
-        #     await asyncio.sleep(3)
         await sio.wait()
     except asyncio.CancelledError:
         print('stop due to `CancelledError`')
