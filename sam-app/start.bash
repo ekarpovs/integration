aefa4df0ad5ae54d67c9b360d61f1660c353fa4b
evgeny <ekarpovsky@gmail.com>
2025-05-27 06:13:30 +0000

    fix scripts

--- sam-app/start.bash
+++ sam-app/start.bash
@@ -5,8 +5,8 @@ echo "Today is " `date`
 echo $1
 #echo $2
 
-docker compose -f docker-compose.yml -p ${USER} up -d
+docker compose -f docker-compose.yml -p intgr-${USER} up -d
 
 if [ "$1" = log ] ; then
-    docker logs -f -t sam-app-${USER}
+    docker logs -f -t sam-app-intgr-${USER}
 fi
--- sam-app/stop.bash
+++ sam-app/stop.bash
@@ -6,7 +6,7 @@ echo $1
 
 if [ "$1" = down ] 
 then
-    docker compose -f docker-compose.yml -p ${USER} down -v
+    docker compose -f docker-compose.yml -p intgr-${USER} down -v
 else
-    docker compose -f docker-compose.yml -p ${USER} stop
+    docker compose -f docker-compose.yml -p intgr-${USER} stop
 fi
